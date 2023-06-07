a77257f476e612b92754f0c290c2814488487378
NileshRamcharan <nileshram@live.nl>
2023-06-07 15:30:22 +0200

    cleaned up code

--- iiatimd_app/lib/views/selection.dart
+++ iiatimd_app/lib/views/selection.dart
@@ -50,102 +50,110 @@ class _SelectedCards extends State<SelectedCards> {
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-
     return ListView.builder(
         itemCount: 3, //list.length
         itemBuilder: (context, index) {
           if (index < 3) {
             //if index = list.length
-            return Padding(
-              padding: const EdgeInsets.fromLTRB(5, 0, 5, 5),
-              child: Container(
-                padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  border: Border.all(color: Colors.white),
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                        width: 75,
-                        height: 75,
-                        color: Colors.blue,
-                        child: Icon(Icons.add_circle_outline_outlined)),
-                    Column(
-                      children: [
-                        Container(
-                            padding: const EdgeInsets.fromLTRB(5, 0, 5, 5),
-                            child: const Text(
-                              'ingredient name', //list.item.title
-                              style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white),
-                            )),
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: SizedBox(
-                            width: screenWidth - 130,
-                            child: const Column(
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: <Widget>[
-                                    Flexible(
-                                      child: Text(
-                                          '  component 1', //list.item.title.component[0]
-                                          textAlign: TextAlign.center,
-                                          style:
-                                              TextStyle(color: Colors.white)),
-                                    ),
-                                    Flexible(
-                                      child: Text(
-                                          'component 2', //list.item.title.component[1]
-                                          textAlign: TextAlign.center,
-                                          style:
-                                              TextStyle(color: Colors.white)),
-                                    )
-                                  ],
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: <Widget>[
-                                    Flexible(
-                                      child: Text(
-                                        'longer component 3', //list.item.title.component[2]
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                                    ),
-                                    Flexible(
-                                      child: Text(
-                                          'super long  component 4', //list.item.title.component[3]
-                                          textAlign: TextAlign.center,
-                                          style:
-                                              TextStyle(color: Colors.white)),
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        const Text('Extra info',
-                            style: TextStyle(color: Colors.white))
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            );
+            return const ActiveCards();
           } else {
             return const AddIngredientCard();
           }
         });
+  }
+}
+
+class ActiveCards extends StatefulWidget {
+  const ActiveCards({super.key});
+
+  @override
+  State<ActiveCards> createState() => _ActiveCardsState();
+}
+
+class _ActiveCardsState extends State<ActiveCards> {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(5, 0, 5, 5),
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          border: Border.all(color: Colors.white),
+        ),
+        child: Row(
+          children: [
+            Container(
+                width: 75,
+                height: 75,
+                color: Colors.blue,
+                child: Icon(Icons.add_circle_outline_outlined)),
+            Column(
+              children: [
+                Container(
+                    padding: const EdgeInsets.fromLTRB(5, 0, 5, 5),
+                    child: const Text(
+                      'ingredient name', //list.item.title
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    )),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SizedBox(
+                    width: screenWidth - 130,
+                    child: const Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Flexible(
+                              child: Text(
+                                  '  component 1', //list.item.title.component[0]
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(color: Colors.white)),
+                            ),
+                            Flexible(
+                              child: Text(
+                                  'component 2', //list.item.title.component[1]
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(color: Colors.white)),
+                            )
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Flexible(
+                              child: Text(
+                                'longer component 3', //list.item.title.component[2]
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            Flexible(
+                              child: Text(
+                                  'super long  component 4', //list.item.title.component[3]
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(color: Colors.white)),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const Text('Extra info', style: TextStyle(color: Colors.white))
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
