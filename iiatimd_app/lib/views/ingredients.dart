df45e260698bef81c21d3c11f6134532480987b9
argelbagel <michielkramers@hotmail.com>
2023-06-07 16:04:47 +0200

    Filter Buttons
    De filter buttons en de logica die niet afhankelijk is van de echte data gemaakt

--- iiatimd_app/lib/views/ingredients.dart
+++ iiatimd_app/lib/views/ingredients.dart
@@ -10,10 +10,8 @@ class IngredientView extends StatelessWidget {
         //test column om real applicatie van card te zien zonder api
         child: Column(
           children: [
-            IngredientCard(name: "Abecaen longfin", effect1: "Weakness to frost", effect2: "Fortify sneak", effect3: "Weakness to poison", effect4: "Fortify restoration", location: "Collected by catching Abacean Longfin fish."),
-            IngredientCard(name: "Blue mountain flower", effect1: "Restore health", effect2: "Fortify Conjuration", effect3: "Fortify health", effect4: "Damage magicka regen", location: "Harvested from the blue variety of Mountain Flower."),
-            IngredientCard(name: "Abecaen longfin", effect1: "Weakness to frost", effect2: "Fortify sneak", effect3: "Weakness to poison", effect4: "Fortify restoration", location: "Collected by catching Abacean Longfin fish."),
-            IngredientCard(name: "Blue mountain flower", effect1: "Restore health", effect2: "Fortify Conjuration", effect3: "Fortify health", effect4: "Damage magicka regen", location: "Harvested from the blue variety of Mountain Flower."),
+            Text("data"),
+            FilterContainer()
           ],
         ),
       )
@@ -87,6 +85,111 @@ class IngredientCard extends StatelessWidget {
           )
            
         ),
+    );
+  }
+}
+
+class Filter extends StatelessWidget {
+  const Filter({super.key, required this.state, required this.onChangeState});
+
+  final String state;
+  final Function(String) onChangeState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        FilterButton(name: "Mushrooms", onChangeState: onChangeState),
+        FilterButton(name: "Plants", onChangeState: onChangeState),
+        FilterButton(name: "Monsters", onChangeState: onChangeState),
+        FilterButton(name: "Animals", onChangeState: onChangeState),
+        FilterButton(name: "Harvestables", onChangeState: onChangeState),
+      ],
+    );
+  }
+}
+
+class FilterButton extends StatelessWidget {
+  const FilterButton({super.key, required this.name, required this.onChangeState});
+
+  final String name;
+  final Function(String) onChangeState;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {onChangeState(name);}, // Image tapped
+      child:Padding(
+        padding: const EdgeInsets.all(3.0),
+        child: Container(decoration:  BoxDecoration(color: Color(0xFF75704E), border: Border.all(color: Colors.black), borderRadius: BorderRadius.all(Radius.circular(7))), child: Padding(
+          padding: const EdgeInsets.all(3.5),
+          child: Text(name, style: const TextStyle(color: Colors.white)),
+        )),
+      ));
+  }
+}
+
+class ListChoice extends StatelessWidget {
+  const ListChoice({super.key, required this.state});
+
+  final String state;
+
+  @override
+  Widget build(BuildContext context) {
+    switch(state){
+      case "Mushrooms":
+        return const Column(
+          children: [
+            IngredientCard(name: "Abecaen longfin", effect1: "Weakness to frost", effect2: "Fortify sneak", effect3: "Weakness to poison", effect4: "Fortify restoration", location: "Collected by catching Abacean Longfin fish."),
+            IngredientCard(name: "Blue mountain flower", effect1: "Restore health", effect2: "Fortify Conjuration", effect3: "Fortify health", effect4: "Damage magicka regen", location: "Harvested from the blue variety of Mountain Flower."),
+            IngredientCard(name: "Abecaen longfin", effect1: "Weakness to frost", effect2: "Fortify sneak", effect3: "Weakness to poison", effect4: "Fortify restoration", location: "Collected by catching Abacean Longfin fish."),
+            IngredientCard(name: "Blue mountain flower", effect1: "Restore health", effect2: "Fortify Conjuration", effect3: "Fortify health", effect4: "Damage magicka regen", location: "Harvested from the blue variety of Mountain Flower."),
+          ],
+        );
+      case "Plants":
+        return const Column(
+          children: [
+            IngredientCard(name: "Abecaen longfin", effect1: "Weakness to frost", effect2: "Fortify sneak", effect3: "Weakness to poison", effect4: "Fortify restoration", location: "Collected by catching Abacean Longfin fish."),
+            IngredientCard(name: "Blue mountain flower", effect1: "Restore health", effect2: "Fortify Conjuration", effect3: "Fortify health", effect4: "Damage magicka regen", location: "Harvested from the blue variety of Mountain Flower."),
+            IngredientCard(name: "Abecaen longfin", effect1: "Weakness to frost", effect2: "Fortify sneak", effect3: "Weakness to poison", effect4: "Fortify restoration", location: "Collected by catching Abacean Longfin fish."),
+            IngredientCard(name: "Blue mountain flower", effect1: "Restore health", effect2: "Fortify Conjuration", effect3: "Fortify health", effect4: "Damage magicka regen", location: "Harvested from the blue variety of Mountain Flower."),
+          ],
+        );
+      case "Monsters":
+        return const Text("f");
+      case "Animals":
+        return const Text(" ");
+      case "Harvestables":
+        return const Text("c");
+      default:
+        return const Text("x");
+    }
+    
+  }
+}
+
+class FilterContainer extends StatefulWidget {
+  const FilterContainer({super.key});
+
+  @override
+  State<FilterContainer> createState() => _FilterContainerState();
+}
+
+class _FilterContainerState extends State<FilterContainer> {
+
+  String state = "Mushrooms";
+
+  void onchangeState(String newState){
+    setState(() => state = newState);
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Filter(state: state, onChangeState: onchangeState),
+        ListChoice(state: state)
+      ],
     );
   }
 }
